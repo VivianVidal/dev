dfaae90f253bcdd323b59e0fc497a23d257a4c43
Vivian Vidal <133300936+VivianVidal@users.noreply.github.com>
2024-12-02 21:44:39 -0300

    Feat(estrutura inicial)

---
+++ AutomacaoKaspper/src/main/java/com/sistema/AutomacaoKaspper/AutomacaoKaspperApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.AutomacaoKaspper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoKaspperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaoKaspperApplication.class, args);
+	}
+
+}
---
+++ AutomacaoKaspper/src/test/java/com/sistema/AutomacaoKaspper/AutomacaoKaspperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.AutomacaoKaspper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomacaoKaspperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
